a51853f2ee102557a79ea56a8d35a5fbb6c97953
GitHub <csy5271abcd@gmail.com>
2025-08-29 00:50:03 +0900

    commit

---
+++ src/test/java/com/mandaggo/mandaggo/MandaggoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandaggo.mandaggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandaggoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
